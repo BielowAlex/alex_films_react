8bb926081da8fddd54b207783326b60f561f3092
BielowAlex <bielow.alex@gmail.com>
2022-06-16 22:54:37 +0300

    edit CartLink.js aded target:_blank for links;

--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -6,7 +6,10 @@ const CartLink = ({movie, selectMovie}) => {
     const {id,original_title,name,first_air_date,release_date,poster_path} = movie;
 
     return (
-        <Link className="movies_cart" to={`/movies/${id}`} onClick={()=>selectMovie(movie)}>
+        <Link
+            className="movies_cart"
+            to={`/movies/${id}`}
+            onClick={()=>selectMovie(movie)} target={"_blank"}>
             <div className="poster">
                 <img width={193} height={280} src={API_IMAGE_URL+poster_path} alt="poster"/>
             </div>
